d78d979ad9d8888c43ea32bd4973de0e80e2feaf
Vladimir Gerasimov <freelancervip@gmail.com>
2026-01-17 14:36:02 +0600

    Heartbeat and sending html

--- index.js
+++ index.js
@@ -8,17 +8,39 @@ const port = process.env.PORT || 10000
 
 const wss = new WebSocket.Server({ server, path: '/ws' })
 
-app.get('/', (req, res) => {
-  res.send('Hello over HTTP!')
+function heartbeat() {
+  this.isAlive = true
+}
+
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 wss.on('connection', (ws) => {
-  console.log('WebSocket client connected')
+  ws.isAlive = true
+  ws.on('error', console.error)
+  ws.on('pong', heartbeat)
 
   ws.on('message', (message) => {
     console.log('Received:', message.toString())
-    ws.send(`Hello over WebSocket!`)
+    ws.send('Hello over WebSocket!')
   })
+})
+
+// Ping all connected clients every 30 seconds
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    // Close connections that failed to "pong" the previous ping
+    if (ws.isAlive === false) return ws.terminate()
+
+    ws.isAlive = false
+    ws.ping()
+  })
+}, 30000)
+
+// Standard shutdown logic
+wss.on('close', function close() {
+  clearInterval(interval)
 })
 
 server.listen(port, () => {
